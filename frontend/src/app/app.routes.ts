0f72262b45be4b1f657335d1ca6f9cc6c5db41fe
SebatianDte <sebateumate@outlook.com>
2025-06-11 19:57:07 -0300

    Componente Confirmar envio

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { CrearEncuestaComponent } from './components/crear-encuesta/crear-encuesta.component';
 import { PresentacionEnlacesComponent } from './components/presentacion-enlaces/presentacion-enlaces.component';
 import { ResponderEncuestaComponent } from './components/responder-encuesta/responder-encuesta.component';
+import { ConfirmarEnvioComponent } from './confirmar-envio/confirmar-envio.component';
+
 export const routes: Routes = [
     {
         path: '',
         component: InicioComponent
     },
     {
-        path: 'crear-encuesta', 
+        path: 'crear-encuesta',
         component: CrearEncuestaComponent
     },
     {
@@ -17,7 +19,11 @@ export const routes: Routes = [
         component: PresentacionEnlacesComponent
     },
     {
-    path: 'responder/:id/:codigo',
-    component: ResponderEncuestaComponent
+        path: 'responder/:id/:codigo',
+        component: ResponderEncuestaComponent
+    },
+    {
+        path: 'confirmar-envio',
+        component: ConfirmarEnvioComponent
     }
 ];
